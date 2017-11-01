d6f22698e6a945131d5ffa2c291167519669259b
ZAHRA SHAKIB <shakib.zsh@gmail.com>
2017-11-01 12:27:19 +0330

    remove names and keys

--- app/src/main/java/com/adpdigital/chabok/starter/StarterApp.java
+++ app/src/main/java/com/adpdigital/chabok/starter/StarterApp.java
@@ -7,14 +7,12 @@ import com.adpdigital.push.AdpPushClient;
 
 public class StarterApp extends Application {
 
-//    private static final String YOUR_APP_ID = "YOUR_APP_ID/SENDER_ID";
-    private static final String YOUR_APP_ID = "torjungaj/1078814764250";
-//    private static final String YOUR_API_KEY = "YOUR_API_KEY";
-    private static final String YOUR_API_KEY = "e3772e0734e26b4ea39a12fe93764db095face28";
-//    private static final String SDK_USERNAME = "SDK_USERNAME";
-    private static final String SDK_USERNAME = "nigitdosvet";
-//    private static final String SDK_PASSWORD = "SDK_PASSWORD";
-    private static final String SDK_PASSWORD = "doiwamahhij";
+    // TODO: use your data from your account in panel
+
+    private static final String YOUR_APP_ID = "YOUR_APP_ID/SENDER_ID";
+    private static final String YOUR_API_KEY = "YOUR_API_KEY";
+    private static final String SDK_USERNAME = "SDK_USERNAME";
+    private static final String SDK_PASSWORD = "SDK_PASSWORD";
 
     private AdpPushClient chabok = null;
 
@@ -35,7 +33,10 @@ public class StarterApp extends Application {
                     SDK_PASSWORD
             );
             chabok.setDevelopment(true);
-            chabok.register("adpdigital.chabok");
+
+            // TODO: use your logic for unique user id
+
+            chabok.register("USER_ID");
         }
     }
 
