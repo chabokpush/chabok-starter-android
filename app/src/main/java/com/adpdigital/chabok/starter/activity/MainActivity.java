ab8d9fcbea6e7eb175dd0415eb2ac1c4c47fdc54
mkouchi <kouchi.mohammad@gmail.com>
2017-11-20 12:00:42 +0330

    add protected apps checking logic for huawei and xiaomi

--- app/src/main/java/com/adpdigital/chabok/starter/activity/MainActivity.java
+++ app/src/main/java/com/adpdigital/chabok/starter/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package com.adpdigital.chabok.starter.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.adpdigital.chabok.starter.R;
 import com.adpdigital.chabok.starter.application.StarterApp;
+import com.adpdigital.chabok.starter.common.DeviceUtil;
 import com.adpdigital.push.AdpPushClient;
 import com.adpdigital.push.Callback;
 import com.adpdigital.push.ConnectionStatus;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FIRST_RUN_PREFERENCES_NAME = "skipProtectedAppsMessage";
+    static boolean shown = false;
     String TAG = "MainActivity";
     private AdpPushClient chabok;
 
@@ -22,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         chabok = ((StarterApp) getApplication()).getPushClient();
 
+        // if deviceManufacturer is huawei or xiaomi, show protected apps guide
+        String deviceManufacturer = DeviceUtil.getDeviceManufacturer().toLowerCase();
+        if(!TextUtils.isEmpty(deviceManufacturer) && (deviceManufacturer.contains("huawei") ||
+                deviceManufacturer.contains("xiaomi"))) {
+            showGuide(deviceManufacturer);
+        }
+    }
+
+    private void showGuide(final String deviceManufacturer) {
+
+        final SharedPreferences preferences = getSharedPreferences("ProtectedApps", Context.MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean(FIRST_RUN_PREFERENCES_NAME, false);
+        if (!isFirstTime && !shown) {
+            showProtectedAppDialog(deviceManufacturer);
+        }
+    }
+
+    private void showProtectedAppDialog(String deviceManufacturer) {
+        shown = true;
+        DeviceUtil.ifHuaweiAlert(this, deviceManufacturer);
     }
 
     @Override
