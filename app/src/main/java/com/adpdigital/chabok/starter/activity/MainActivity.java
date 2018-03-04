2bb507f6cfdc91e41571435775c82f0d378d721a
ZAHRA SHAKIB <shakib.zsh@gmail.com>
2018-03-04 21:17:00 +0330

    add logcat export command

--- app/src/main/java/com/adpdigital/chabok/starter/activity/MainActivity.java
+++ app/src/main/java/com/adpdigital/chabok/starter/activity/MainActivity.java
@@ -1,8 +1,18 @@
 package com.adpdigital.chabok.starter.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.adpdigital.chabok.starter.R;
@@ -11,25 +21,101 @@ import com.adpdigital.push.AdpPushClient;
 import com.adpdigital.push.Callback;
 import com.adpdigital.push.ConnectionStatus;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity";
     private AdpPushClient chabok;
+    private Process logcat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         chabok = ((StarterApp) getApplication()).getPushClient();
 
-
         if (chabok.hasProtectedAppSupport()) {
-
             chabok.showProtectedAppSettings(MainActivity.this, getString(R.string.app_name), null, null);
         }
 
+        final Button sendLog = (Button) findViewById(R.id.send_log);
+        sendLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isExternalStorageWritable()) {
+                    createAndShareLogFile();
+                }
+
+            }
+        });
+
     }
 
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    private void createAndShareLogFile() {
+
+
+//        int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        String[] permissions = {
+//                "android.permission.WRITE_EXTERNAL_STORAGE"
+//        };
+//        if (permission != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            if (shouldAskPermissions()) {
+//                ActivityCompat.requestPermissions(
+//                        MainActivity.this,
+//                        permissions,
+//                        200
+//                );
+//            }
+//
+//        }
+
+        File logFile = new File(getExternalFilesDir(null), "chaboklog.txt");
+
+        try {
+
+            boolean result = logFile.createNewFile();
+
+
+            if (result) {
+
+                String cmd = "logcat -c -d -f" + logFile.getAbsolutePath();
+                Runtime.getRuntime().exec(cmd);
+
+//                shareFile(MainActivity.this, logFile.getAbsoluteFile());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public boolean shouldAskPermissions() {
+//        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+//    }
+
+
+    private void shareFile(Context context, File file) {
+
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/*");
+//        sharingIntent.setPackage("org.telegram.messenger");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + file.getAbsolutePath()));
+        context.startActivity(Intent.createChooser(sharingIntent, "share file with"));
+
+    }
 
     @Override
     protected void onResume() {
