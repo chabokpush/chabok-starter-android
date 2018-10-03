d056c389cfcae5cf8166f513ce76841bad4ca179
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2018-10-03 10:34:57 +0330

    Add notificationHandler listener and notification data handler

--- app/src/main/java/com/adpdigital/chabok/starter/application/StarterApp.java
+++ app/src/main/java/com/adpdigital/chabok/starter/application/StarterApp.java
@@ -1,17 +1,23 @@
 package com.adpdigital.chabok.starter.application;
 
 import android.util.Log;
+import android.os.Bundle;
+import org.json.JSONObject;
 import android.app.Application;
 import com.adpdigital.push.PushMessage;
 import com.adpdigital.push.AdpPushClient;
+import com.adpdigital.push.ChabokNotification;
+import com.adpdigital.push.NotificationHandler;
+import android.support.v4.app.NotificationCompat;
 import com.adpdigital.chabok.starter.activity.MainActivity;
+import static com.adpdigital.chabok.starter.common.Constants.YOUR_APP_ID;
 import static com.adpdigital.chabok.starter.common.Constants.SDK_PASSWORD;
 import static com.adpdigital.chabok.starter.common.Constants.SDK_USERNAME;
 import static com.adpdigital.chabok.starter.common.Constants.YOUR_API_KEY;
-import static com.adpdigital.chabok.starter.common.Constants.YOUR_APP_ID;
 
 public class StarterApp extends Application {
 
+    private final String TAG = this.getClass().getName();
     private AdpPushClient chabok = null;
 
     @Override
@@ -37,8 +43,45 @@ public class StarterApp extends Application {
                     SDK_PASSWORD
             );
             chabok.setDevelopment(true);
-
             chabok.addListener(this);
+            chabok.addNotificationHandler(getNotificationHandler());
+        }
+    }
+
+    private NotificationHandler getNotificationHandler(){
+        return new NotificationHandler(){
+
+            @Override
+            public Class getActivityClass(ChabokNotification chabokNotification) {
+                // return preferred activity class to be opened on this message's notification
+                return MainActivity.class;
+            }
+
+            @Override
+            public boolean buildNotification(ChabokNotification chabokNotification, NotificationCompat.Builder builder) {
+                // use builder to customize the notification object
+                // return false to prevent this notification to be shown to the user, otherwise true
+                getDataFromChabokNotification(chabokNotification);
+                return true;
+            }
+        };
+    }
+
+    private void getDataFromChabokNotification(ChabokNotification chabokNotification) {
+        if (chabokNotification != null) {
+            if (chabokNotification.getExtras() != null) {
+                Bundle payload = chabokNotification.getExtras();
+
+                //FCM message data is here
+                Object data = payload.get("data");
+                Log.d(TAG, "getDataFromChabokNotification: The ChabokNotification data is : " + String.valueOf(data));
+            } else if (chabokNotification.getMessage() != null) {
+                PushMessage payload = chabokNotification.getMessage();
+
+                //Chabok message data is here
+                JSONObject data = payload.getData();
+                Log.d(TAG, "getDataFromChabokNotification: The ChabokNotification data is : " + data);
+            }
         }
     }
 
