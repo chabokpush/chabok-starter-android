d14d353669136bb8e252a7e59b21c8e45a9c9d10
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-06-26 11:37:48 +0430

    Change registration page for show deep-link

--- app/src/main/java/com/adpdigital/chabok/starter/activity/RegisterActivity.java
+++ app/src/main/java/com/adpdigital/chabok/starter/activity/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.adpdigital.chabok.starter.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.adpdigital.chabok.starter.R;
 import com.adpdigital.chabok.starter.common.Constants;
@@ -13,12 +15,15 @@ import com.adpdigital.push.AdpPushClient;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextView lblDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         final EditText userId = (EditText) findViewById(R.id.user_id);
         Button registerBtn = (Button) findViewById(R.id.register_btn);
+
+        lblDescription = (TextView) findViewById(R.id.textView);
 
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +47,18 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+        Intent intent = getIntent();
+        AdpPushClient.get().appWillOpenUrl(intent.getData());
+        lblDescription.setText("Welcome to the Chabok starter project, App opened by deep-link = " + intent.getData().toString());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        Uri data = intent.getData();
+        AdpPushClient.get().appWillOpenUrl(data);
+        lblDescription.setText("Welcome to the Chabok starter project. App opened by deep-link = " + data.toString());
     }
 
 }
